23534bd81be2c85cd2ff1b9388b43aab8d8abe38
abdo-shabrawy <abdobassiony996@hotmail.com>
2016-08-05 12:01:06 +0200

    Update Max_Profit_Subset(No Two Adjacent Nodes).java

--- Max_Profit_Subset(No Two Adjacent Nodes).java
+++ Max_Profit_Subset(No Two Adjacent Nodes).java
@@ -2,7 +2,8 @@ static ArrayList<Integer> adjList [];
 	static int dp1 [];
 	static int dp2 [];
 	static int earn [];
-	static void dfs (int V , int pV) { // ans is max(dp1[1] , dp2[1])
+	// O(N)
+	static void dfs (int V , int pV) { // ans is max(dp1[1] , dp2[1]) 
 		int sum1 = 0;
 		int sum2 = 0;
 		
